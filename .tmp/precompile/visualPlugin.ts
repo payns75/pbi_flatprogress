edf1e264b1324b6087da2a772fb8258b73010782
JACQUEMIN Remy (BPRI) <RIJACRE@dom020.ibp>
2018-09-27 17:26:12 +0200

    compile

---
+++ .tmp/precompile/visualPlugin.ts
@@ -0,0 +1,11 @@
+module powerbi.visuals.plugins {
+    export var pbiflatprogress111DDC2C0F0D0384236A63C11C134C5CDB5_DEBUG = {
+        name: 'pbiflatprogress111DDC2C0F0D0384236A63C11C134C5CDB5_DEBUG',
+        displayName: 'pbi_flatprogress_1_11',
+        class: 'Visual',
+        version: '1.0.0',
+        apiVersion: '1.11.0',
+        create: (options: extensibility.visual.VisualConstructorOptions) => new powerbi.extensibility.visual.pbiflatprogress111DDC2C0F0D0384236A63C11C134C5CDB5.Visual(options),
+        custom: true
+    };
+}
